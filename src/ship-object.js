546c2a0ee6d9478befcc09a0b7b0a0e33f9c86a7
tgorsk15 <tgbusiness463@gmail.com>
2024-03-23 17:30:03 -0500

    create ship Object and gameboard file

---
+++ src/ship-object.js
@@ -0,0 +1,7 @@
+class Ship {
+    constructor(length, hits, isSunk) {
+        this.length = length;
+        this.hits = hits;
+        this.isSunk = isSunk;
+    }
+}
--- src/test.js
+++
@@ -1,5 +0,0 @@
-
-
-export function testFunc() {
-    console.log('yes maam')
-}
