d1150aa65f5a858dab880ceb4fb0967a33ba4d07
tgorsk15 <tgbusiness463@gmail.com>
2024-03-29 12:57:42 -0500

    build out renderGameBoard and attach buildGrid()
    These two functions will primarly be responsible for creating the gameboards
    section of the webpage,and thus rendering each player's grid

--- src/gameLoop.js
+++ src/gameLoop.js
@@ -1,20 +1,25 @@
 /* eslint-disable import/prefer-default-export */
 import { Player, userPlayer, computerPlayer } from "./player";
 import { gameBoardController } from "./gameboardController";
-import { createFleet, createOppFleet } from "./ship-object"
+import { createFleet, createOppFleet } from "./ship-object";
+import { domManipulation } from "./userInterface";
 
 export const initializeGame = function createGame() {
-    const humanPlayer = new Player
+    const runDOM = domManipulation();
+
+    const humanPlayer = new Player('Player 1')
     const humanFleet = createFleet()
     humanPlayer.gameBoard = gameBoardController(humanFleet);
     const humanBoard = humanPlayer.gameBoard
-    humanBoard.createBoard();
+    runDOM.renderGameBoard(humanBoard.createBoard(), humanPlayer.player);
 
-    const AIplayer = new Player;
+
+    const AIplayer = new Player('Player 2');
     const computerFleet = createOppFleet();
     AIplayer.gameBoard = gameBoardController(computerFleet);
     const computerBoard = AIplayer.gameBoard;
     computerBoard.createBoard();
+    // render gameBoard here
 
-    // call renderGameBoard function here?
+    // call render dialogue box here
 }
--- src/userInterface.js
+++ src/userInterface.js
@@ -1,5 +1,52 @@
+/* eslint-disable import/prefer-default-export */
 
 
-const initialLoad = function() {
-    
+export const initialLoad = function() {
+
+}
+
+
+export const domManipulation = function () {
+    const playerBoards = document.querySelector('.gameboards');
+
+    function renderGameBoard(board, playerName) {
+        // console.log(board);
+        const gameBoardWrapper = document.createElement('div');
+        appendElement(gameBoardWrapper, 'board-wrapper', playerBoards)
+       
+        const boardTitle = document.createElement('h2');
+        appendElement(boardTitle, 'board-title', gameBoardWrapper);
+        boardTitle.textContent = playerName;
+
+        // render board:
+        const gameboard = document.createElement('div');
+        appendElement(gameboard, 'gameboard', gameBoardWrapper);
+
+        buildGrid(gameboard, board);
+    }
+
+    function buildGrid(gameboardElement, board) {
+        for (let i = 1; i < 11; i++) {
+            const row = document.createElement('div');
+            appendElement(row, 'row', gameboardElement);
+
+            for (let j = 1; j < 11; j++) {
+                const cell = document.createElement('div');
+                cell.coordinates = [i, j];
+                console.log(cell.coordinates)
+                appendElement(cell, 'cell', row);
+            }
+        }
+
+    }
+
+    function appendElement(elementName, className, fatherElement ) {
+        elementName.classList.add(className);
+        fatherElement.appendChild(elementName);
+
+        return elementName;
+    }
+
+    return {renderGameBoard, appendElement}
+
 }
