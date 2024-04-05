970564866759a7209feb81a147cef59f3bc1cc9b
tgorsk15 <tgbusiness463@gmail.com>
2024-04-04 19:34:19 -0500

    Fix ship placement error
    Did this by moving the location of the 'click' event Listener to outside
    of cellHover()

--- src/shipPlacement.js
+++ src/shipPlacement.js
@@ -26,25 +26,32 @@ export const humanShipPlacement = function (humanBoard, ships) {
             cell.addEventListener('mouseover', () => {
                 cellHover(cell, ships[shipIndex])
             });
-            // cell.addEventListener('mouseout', () => {
-            //     cell.classList.remove('valid-placement', 'invalid-placement');
-            // })
-            // cell.addEventListener('click', () => {
-            //     console.log(cell.id)
-            // });
+
+            cell.addEventListener('click', () => {
+                console.log(cell.activeCells);
+                if (cell.classList.contains('valid-placement')) {
+                    placeHorizontally(cell.coordinates, cell.activeCells, ships[shipIndex]);
+                    shipIndex += 1;
+                    console.log('new ship');
+                    console.log(shipIndex);
+                }
+                return shipIndex
+            })
         })
     // }
     
     function cellHover(cell, ship) {
         console.log(ship);
         const cellCoords = cell.coordinates;
-        const activeCells = [];
+        cell.activeCells = [];
+        const groupedCells = cell.activeCells;
+        console.log(cell.activeCells)
         // have to check if its horizontal or vertical
         // then check if starting point + ship length is valid
 
-        // if (shipIndex === 6) {
-        //     return
-        // }
+        if (shipIndex === 5) {
+            return
+        }
 
         if (currentPlane === 'horizontal') {
             const cellRow = cellCoords[0]
@@ -52,46 +59,46 @@ export const humanShipPlacement = function (humanBoard, ships) {
             
             for (let i = 0; i < ship.length; i++) {
                 const activeCell = document.getElementById(`${cellRow} ${cellColumn}h`)
-                activeCells.push(activeCell);
+                groupedCells.push(activeCell);
                 cellColumn += 1
                 if (cellColumn > 10) {
                     break
                 }
             }
-            console.log(activeCells);
+            console.log(groupedCells);
 
             if ((cellCoords[1] + ship.length) - 1 <= 10 ) {
                 console.log('this is valid!')
-                activeCells.forEach((elem) => {
+                groupedCells.forEach((elem) => {
                    elem.classList.add('valid-placement'); 
                 })
                 
             } else if ((cellCoords[1] + ship.length) - 1 > 10){
                 console.log('not valid');
-                activeCells.forEach((elem) => {
+                groupedCells.forEach((elem) => {
                     elem.classList.add('invalid-placement'); 
                 })
             }
 
             cell.addEventListener('mouseout', () => {
-                activeCells.forEach((elem) => {
+                groupedCells.forEach((elem) => {
                     elem.classList.remove('valid-placement', 'invalid-placement')
                 })
             })
 
-            cell.addEventListener('click', () => {
-                console.log(activeCells);
-                if (cell.classList.contains('valid-placement')) {
-                    placeHorizontally(cellCoords, activeCells, ship);
-                    shipIndex += 1;
-                    console.log('new ship');
-                    console.log(shipIndex);
-                }
-                return shipIndex
-            })
 
         } else if (currentPlane === 'vertical') {
+            let cellRow = cellCoords[0]
+            const cellColumn = cellCoords[1];
 
+            for (let i = 0; i < ship.length; i++) {
+                const activeCell = document.getElementById(`${cellRow} ${cellColumn}h`)
+                groupedCells.push(activeCell);
+                cellRow += 1
+                if (cellRow > 10) {
+                    break
+                }
+            }
 
         }
     }
