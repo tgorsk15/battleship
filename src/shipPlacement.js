51deff94cd577ed4f796f449545d3a2f0f96a80f
tgorsk15 <tgbusiness463@gmail.com>
2024-04-04 20:03:59 -0500

    Add functionality for placing vertical ships on board

--- src/shipPlacement.js
+++ src/shipPlacement.js
@@ -14,7 +14,7 @@ export const humanShipPlacement = function (humanBoard, ships) {
     const occupiedCells = [];
 
     // sets plane
-    const currentPlane = 'horizontal';
+    const currentPlane = 'vertical';
     const humanCells = document.querySelectorAll('.cell');
     
     const allShipsPlaced = false;
@@ -30,10 +30,16 @@ export const humanShipPlacement = function (humanBoard, ships) {
             cell.addEventListener('click', () => {
                 console.log(cell.activeCells);
                 if (cell.classList.contains('valid-placement')) {
-                    placeHorizontally(cell.coordinates, cell.activeCells, ships[shipIndex]);
-                    shipIndex += 1;
-                    console.log('new ship');
-                    console.log(shipIndex);
+                    if (currentPlane === 'horizontal') {
+                        placeHorizontally(cell.coordinates, cell.activeCells, ships[shipIndex]);
+                        shipIndex += 1;
+                        console.log(shipIndex);
+                    } else if (currentPlane === 'vertical') {
+                        placeVertically(cell.coordinates, cell.activeCells, ships[shipIndex]);
+                        shipIndex += 1;
+                        console.log(shipIndex);
+                    }
+                    
                 }
                 return shipIndex
             })
@@ -45,7 +51,6 @@ export const humanShipPlacement = function (humanBoard, ships) {
         const cellCoords = cell.coordinates;
         cell.activeCells = [];
         const groupedCells = cell.activeCells;
-        console.log(cell.activeCells)
         // have to check if its horizontal or vertical
         // then check if starting point + ship length is valid
 
@@ -99,18 +104,50 @@ export const humanShipPlacement = function (humanBoard, ships) {
                     break
                 }
             }
+            console.log(groupedCells);
+
+            if ((cellCoords[0] + ship.length) - 1 <= 10 ) {
+                console.log('this is valid!')
+                groupedCells.forEach((elem) => {
+                   elem.classList.add('valid-placement'); 
+                })
+                
+            } else if ((cellCoords[0] + ship.length) - 1 > 10){
+                console.log('not valid');
+                groupedCells.forEach((elem) => {
+                    elem.classList.add('invalid-placement'); 
+                })
+            }
+
+            cell.addEventListener('mouseout', () => {
+                groupedCells.forEach((elem) => {
+                    elem.classList.remove('valid-placement', 'invalid-placement')
+                })
+            });
 
         }
     }
 
     function placeHorizontally(cellCoords, activeCells, ship) {
         activeCells.forEach((elem) => {
-            console.log(activeCells)
             console.log(elem.coordinates);
             occupiedCells.push(elem.coordinates);
             elem.classList.add('placed')
         });
+        // maybe put trigger in here to check if all ships are placed
+        // if true, disbale pointer events and run function for
+        // placing computer ships
         humanBoard.placeHorizontalShip(cellCoords[0], cellCoords[1], ship);
+        console.log(occupiedCells)
+    }
+
+    function placeVertically(cellCoords, activeCells, ship) {
+        activeCells.forEach((elem) => {
+            console.log(elem.coordinates);
+            occupiedCells.push(elem.coordinates);
+            elem.classList.add('placed')
+        });
+        humanBoard.placeVerticalShip(cellCoords[0], cellCoords[1], ship);
         console.log(occupiedCells)
     }
 
